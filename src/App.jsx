96b18b66107872264083e511dc25ba8cbf8538d3
Rodrigo <rodrigo.ibaceta01@gmail.com>
2021-12-04 11:53:19 -0300

    setup PDF

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { PDFViewer } from '@react-pdf/renderer';
+import MyDocument from './MyDocument';
 
 const App = function () {
   return (
-    <div className="App" />
+    <div className="App">
+      <PDFViewer style={{ width: '100%', height: '98vh' }}>
+        <MyDocument />
+      </PDFViewer>
+    </div>
   );
 };
 
